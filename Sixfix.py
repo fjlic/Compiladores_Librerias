07cd8602cccbf525e618a6cee50b5cc5b3814d5f
karlairungaray <65935744+karlairungaray@users.noreply.github.com>
2020-06-19 13:54:43 -0500

    Update Sixfix.py

--- Sixfix.py
+++ Sixfix.py
@@ -1,4 +1,4 @@
-# Description: program converts infix to postfix notation
+# Description: program converts infix to postfix and prefix notation
 # https://gist.github.com/awadalaa/7ef7dc7e41edb501d44d1ba41cbf0dc6
 # https://www.web4college.com/converters/infix-to-postfix-prefix.php
 # https://www.infor.uva.es/~cvaca/asigs/AlgInfPost.htm
@@ -28,7 +28,11 @@ class InfixConverter:
     def __init__(self):
         self.stack = Stack()
         self.precedence = {'+':1, '-':1, '*':2, '/':2, '^':3}
-
+		
+# define fucntion that is used to assign precendence to operator.
+# Here ^ denotes exponent operator.
+# In this fucntion we assume that higher integer value
+# means higher precendence */
     def hasLessOrEqualPriority(self, a, b):
         if a not in self.precedence:
             return False
@@ -53,22 +57,27 @@ class InfixConverter:
         expr = expr.replace(" ", "")
         self.stack = Stack()
         output = ''
-
+#  run loop till end of infix expression 
         for c in expr:
             if self.isOperand(c):
+			    #add operand symbol to postfix expr
                 output += c
             else:
                 if self.isOpenParenthesis(c):
                     self.stack.push(c)
                 elif self.isCloseParenthesis(c):
                     operator = self.stack.pop()
+					#pop and keep popping until 
+					#'(' encounterd 
                     while not self.isOpenParenthesis(operator):
                         output += operator
                         operator = self.stack.pop()              
                 else:
                     while (not self.stack.isEmpty()) and self.hasLessOrEqualPriority(c,self.stack.peek()):
-                        output += self.stack.pop()
+                        output += self.stack.pop()		# add them to postfix expresion 
                     self.stack.push(c)
+					#because just above while loop will terminate we have oppped one extra item
+					#for which condition fails and loop terminates, so that one
         while (not self.stack.isEmpty()):
             output += self.stack.pop()
         return output
